f561c76ca12f339c31e6f6a44543ecaae9edad44
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-02-24 18:03:44 +0800

    feat: footer dmq

---
+++ src/components/footer-dmq/index.tsx
@@ -0,0 +1,55 @@
+import type { ContentBlockRegistry } from '@/hooks/local/use-content-blocks';
+import { createMenuFromDescription } from '@/utils';
+import { PortableText } from 'next-sanity';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+export default function FooterDmq({ block }: ContentBlockRegistry) {
+  if (!block) return null;
+
+  const menus = createMenuFromDescription({
+    description: block?.listItems?.find((e) => e.slug?.current === 'menus')?.description,
+  });
+
+  const desc = block?.listItems?.find((e) => e.slug?.current === 'title');
+
+  return (
+    <article id={block.slug?.current}>
+      <div className="main-padding">
+        <div className="component-wrapper">
+          <section className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center">
+            <section className="space-y-2 max-md:text-center">
+              <PortableText value={desc?.customAttributes?.find((e) => e.key === 'header')?.description ?? []} />
+            </section>
+            <header className="md:col-span-2 max-md:text-center">
+              <PortableText value={desc?.description ?? []} />
+            </header>
+          </section>
+        </div>
+      </div>
+
+      <footer className="bg-black text-white">
+        <div className="main-padding-x py-6">
+          <div className="component-wrapper flex justify-between flex-wrap gap-4 md:gap-6 max-md:items-center max-md:justify-center">
+            {<PortableText value={block.description ?? []} />}
+            <ul className="flex">
+              {menus?.map((e, i) => {
+                const lastIndex = i === menus?.length - 1;
+                return (
+                  <Fragment key={e.label}>
+                    <li>
+                      <Link href={e.href} className="hover:underline">
+                        {e.label}
+                      </Link>
+                    </li>
+                    {lastIndex ? null : <li className="px-2 md:px-4">|</li>}
+                  </Fragment>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </article>
+  );
+}
